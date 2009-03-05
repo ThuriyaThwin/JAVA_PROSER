3790b398457df93944e90a22647b45dc5a4dfa53
2elad.levi@gmail.com <2elad.levi@gmail.com@cd73980e-07cc-11de-bc6d-132d5dcf21d4>
2009-03-05 16:38:22 +0000

    dsa-a v1 not tested yet

---
+++ src/DSA_A_Agent.java
@@ -0,0 +1,17 @@
+
+public class DSA_A_Agent extends DSAAgent{
+	
+	
+	public DSA_A_Agent(int id, Problem problem, int max_cycles,  AbstractAgent agents_table[]) {
+		super(id, problem, max_cycles, (AbstractAgent[]) agents_table);
+	}
+
+	void select_next_value(boolean is_improve, int v, double p){
+		if (is_improve){
+			/* delta > 0 */
+			change_value_with_prob(v,p);
+		}	
+		
+	}
+	
+}

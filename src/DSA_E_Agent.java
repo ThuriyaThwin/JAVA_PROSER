fc0916007c09bf0051d29d77c58b6db8f2edf44f
yossi.miriam <yossi.miriam@cd73980e-07cc-11de-bc6d-132d5dcf21d4>
2009-03-11 12:17:55 +0000

    This is a non crashing version with Anytime However it is wrong (BAD results with DBA ...)

--- src/DSA_E_Agent.java
+++ src/DSA_E_Agent.java
@@ -1,8 +1,8 @@
 
 public class DSA_E_Agent extends DSAAgent{
 	
-	public DSA_E_Agent(int id, Problem problem, int max_cycles,  AbstractAgent agents_table[]) {
-		super(id, problem, max_cycles, (AbstractAgent[]) agents_table);
+	public DSA_E_Agent(int id, Problem problem, int max_cycles,  AbstractAgent agents_table[], boolean any_time) {
+		super(id, problem, max_cycles, (AbstractAgent[]) agents_table, any_time);
 	}
 
 	void select_next_value(boolean is_improve, int v, double p){
--- src/MessageOKAnyTime2Son.java
+++ src/MessageOKAnyTime2Son.java
@@ -1,9 +1,9 @@
 
 // This is the message the will be sent to BFS child when Any Time is implemented
-public class MessageOKAny2TimeSon extends MessageOK {
+public class MessageOKAnyTime2Son extends MessageOK {
 	public int best_index;
 	
-	public MessageOKAny2TimeSon(int id, int current_value, int best_index) {		
+	public MessageOKAnyTime2Son(int id, int current_value, int best_index) {		
 		super(id, current_value); 
 		this.best_index = best_index;
 	}

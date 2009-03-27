c6009d12045cf7c3326c9a957a881bec3cf3ee9d
yossi.miriam <yossi.miriam@cd73980e-07cc-11de-bc6d-132d5dcf21d4>
2009-03-27 14:37:11 +0000

    Added support for any time automatic termination (when result was reached)

--- trunk/src/MessageOK.java
+++ trunk/src/MessageOK.java
@@ -1,18 +1,12 @@
-/**
- * OK message for DBA (Will be renamed if can't be used by DSA)
- */
 
 public class MessageOK {
 
 	public int id; // the id of sender
 	public int current_value; // the current value of sender
-	// TODO for debug
-	int step_no;
 	
-	public MessageOK(int id, int current_value, int step_no) {
+	public MessageOK(int id, int current_value) {
 		this.current_value = current_value;
 		this.id = id;
-		this.step_no = step_no;
 
 	}
 
--- trunk/src/MessageOKAnyTime2Parent.java
+++ trunk/src/MessageOKAnyTime2Parent.java
@@ -1,10 +1,10 @@
 
-public class MessageOKAnyTime2Parent extends MessageOK {
+public class MessageOKAnyTime2Parent extends MessageOKAnyTime {
 	public int cost_i;
 	public int step_no;
 	
-	public MessageOKAnyTime2Parent (int id, int current_value, int cost_i, int step_no, int real_step_no) {		
-		super(id, current_value, real_step_no); 
+	public MessageOKAnyTime2Parent (int id, int current_value, int cost_i, int step_no, boolean terminate) {		
+		super(id, current_value, terminate); 
 		this.cost_i = cost_i;
 		this.step_no = step_no;
 	}
